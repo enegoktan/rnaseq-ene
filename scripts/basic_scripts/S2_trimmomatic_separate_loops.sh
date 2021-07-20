df51514cbb6ec5a0a4df6587b3ffa99f948e5432
enegoktan <goktan.ene@gmail.com>
2021-07-20 18:15:44 +0100

    copy files in

--- scripts/basic_scripts/S2_trimmomatic_separate_loops.sh
+++ scripts/basic_scripts/S2_trimmomatic_separate_loops.sh
@@ -9,27 +9,27 @@
 # Load modules
 	module purge; module load bluebear
 	module load bear-apps-unsupported/handbuilt/2017
-	module load apps/trimmomatic/v0.32
+	module load Trimmomatic/0.39-Java-11
 	module load bear-apps-unsupported/2017a
-	
+
 # go to directory
 	cd /rds/projects/t/thomaspz-fa-rna-seq/Avrahami/Fastq
 
 #Clip adapters
-	sample="SRR12758062_GSM4813679_67788_AEHL151_Homo_sapiens_RNA-Seq.fastq SRR12758063_GSM4813679_67788_AEHL151_Homo_sapiens_RNA-Seq.fastq SRR12758064_GSM4813680_67789_AEHL151_Homo_sapiens_RNA-Seq.fastq SRR12758065_GSM4813680_67789_AEHL151_Homo_sapiens_RNA-Seq.fastq SRR12758066_GSM4813681_67791_AEHL151_Homo_sapiens_RNA-Seq.fastq SRR12758067_GSM4813681_67791_AEHL151_Homo_sapiens_RNA-Seq.fastq SRR12758068_GSM4813682_67792_AEHL151_Homo_sapiens_RNA-Seq.fastq SRR12758069_GSM4813682_67792_AEHL151_Homo_sapiens_RNA-Seq.fastq" 
-	
+	sample="SRR12758062_GSM4813679_67788_AEHL151_Homo_sapiens_RNA-Seq.fastq SRR12758063_GSM4813679_67788_AEHL151_Homo_sapiens_RNA-Seq.fastq SRR12758064_GSM4813680_67789_AEHL151_Homo_sapiens_RNA-Seq.fastq SRR12758065_GSM4813680_67789_AEHL151_Homo_sapiens_RNA-Seq.fastq SRR12758066_GSM4813681_67791_AEHL151_Homo_sapiens_RNA-Seq.fastq SRR12758067_GSM4813681_67791_AEHL151_Homo_sapiens_RNA-Seq.fastq SRR12758068_GSM4813682_67792_AEHL151_Homo_sapiens_RNA-Seq.fastq SRR12758069_GSM4813682_67792_AEHL151_Homo_sapiens_RNA-Seq.fastq"
+
 	for r1 in $sample; do
 	r2=$r1
 	r2="${r1/RNA-Seq/RNA-Seq.1}"
+
 	java -jar ${TMATIC_ROOT}/trimmomatic-0.32.jar SE -trimlog Triming_LOG_trimmomatic1.fastq $r1 $r2 ILLUMINACLIP:contaminant_list_polyA.fa:3:10:8:6 LEADING:3 TRAILING:3
 	done
 
 #Clip polyA tails
-	sample="SRR12758062_GSM4813679_67788_AEHL151_Homo_sapiens_RNA-Seq.1.fastq SRR12758063_GSM4813679_67788_AEHL151_Homo_sapiens_RNA-Seq.1.fastq SRR12758064_GSM4813680_67789_AEHL151_Homo_sapiens_RNA-Seq.1.fastq SRR12758065_GSM4813680_67789_AEHL151_Homo_sapiens_RNA-Seq.1.fastq SRR12758066_GSM4813681_67791_AEHL151_Homo_sapiens_RNA-Seq.1.fastq SRR12758067_GSM4813681_67791_AEHL151_Homo_sapiens_RNA-Seq.1.fastq SRR12758068_GSM4813682_67792_AEHL151_Homo_sapiens_RNA-Seq.1.fastq SRR12758069_GSM4813682_67792_AEHL151_Homo_sapiens_RNA-Seq.1.fastq" 
-	
+	sample="SRR12758062_GSM4813679_67788_AEHL151_Homo_sapiens_RNA-Seq.1.fastq SRR12758063_GSM4813679_67788_AEHL151_Homo_sapiens_RNA-Seq.1.fastq SRR12758064_GSM4813680_67789_AEHL151_Homo_sapiens_RNA-Seq.1.fastq SRR12758065_GSM4813680_67789_AEHL151_Homo_sapiens_RNA-Seq.1.fastq SRR12758066_GSM4813681_67791_AEHL151_Homo_sapiens_RNA-Seq.1.fastq SRR12758067_GSM4813681_67791_AEHL151_Homo_sapiens_RNA-Seq.1.fastq SRR12758068_GSM4813682_67792_AEHL151_Homo_sapiens_RNA-Seq.1.fastq SRR12758069_GSM4813682_67792_AEHL151_Homo_sapiens_RNA-Seq.1.fastq"
+
 	for r1 in $sample; do
 	r2=$r1
 	r2="${r1/RNA-Seq.1/RNA-Seq.2}"
 	java -jar ${TMATIC_ROOT}/trimmomatic-0.32.jar SE -trimlog Triming_LOG_Trimmomatic2.fastq $r1 $r2 ILLUMINACLIP:polyA.fa:3:10:8:6 SLIDINGWINDOW:4:15 MINLEN:30
 	done
-	
--- scripts/diedisheim/pre_processing/2.trimmomatic_RNAseq.sh
+++ scripts/diedisheim/pre_processing/2.trimmomatic_RNAseq.sh
@@ -12,8 +12,6 @@ module load FastQC/0.11.9-Java-11
 module load MultiQC/1.9-foss-2019b-Python-3.7.4
 
 
-# Trimming test.fastq
-
 # Trim in two steps - first take off the adapters, then take off the polyA and the quality.
 cd /rds/projects/t/thomaspz-fa-rna-seq/Ene/data/diedisheim/dataset/test_trim
 
@@ -40,11 +38,9 @@ done
 #java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar PE -trimlog Triming_LOG_trimmomatic1.fastq  -basein ${file} -baseout ${file}temp.fastq ILLUMINACLIP:/rds/projects/t/thomaspz-fa-rna-seq/Ene/scripts_and_manuals/scripts/Additional_files/contaminant_list_polyA.fa:3:10:8:6 LEADING:3 TRAILING:3
 #java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar PE -trimlog Triming_LOG_Trimmomatic2.fastq  temp_1P.fastq temp_2P.fastq -baseout trimTC${file} ILLUMINACLIP:/rds/projects/t/thomaspz-fa-rna-seq/Ene/scripts_and_manuals/scripts/Additional_files/polyA.fa:3:10:8:6 SLIDINGWINDOW:4:15 MINLEN:30
 
-
 #then test the trimmed and clipped file
 gunzip *RNA-Seq_trimmed*
 
-# for loop in bash to repeat for each file in the folder
 for file in *RNA-Seq_trimmed*
 do
   fastqc ${file}  -o /rds/projects/t/thomaspz-fa-rna-seq/Ene/data/diedisheim/2.trimmed_fastQC

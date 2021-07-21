9c96e2f1d0c2e27fd77017ca82a34357a5052c5f
enegoktan <goktan.ene@gmail.com>
2021-07-21 14:31:53 +0100

    separate three sets of five files for test_trim

--- scripts/diedisheim/pre_processing/2.trimmomatic_RNAseq.sh
+++ scripts/diedisheim/pre_processing/2.trimmomatic_RNAseq.sh
@@ -16,8 +16,15 @@ module load MultiQC/1.9-foss-2019b-Python-3.7.4
 cd /rds/projects/t/thomaspz-fa-rna-seq/Ene/data/diedisheim/dataset/test_trim
 
 # Clip adapters
+# analyse files by sets of five
+
 #sample=*RNA-Seq_1.fastq.gz
-sample="SRR5998588_GSM2769683_Ctrl_0h_1_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998589_GSM2769684_Ctrl_0h_2_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998590_GSM2769685_Ctrl_0h_3_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998591_GSM2769686_Ctrl_4h_1_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998592_GSM2769687_Ctrl_4h_2_Homo_sapiens_RNA-Seq_1.fastq.gz"
+# first five
+#sample="SRR5998588_GSM2769683_Ctrl_0h_1_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998589_GSM2769684_Ctrl_0h_2_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998590_GSM2769685_Ctrl_0h_3_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998591_GSM2769686_Ctrl_4h_1_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998592_GSM2769687_Ctrl_4h_2_Homo_sapiens_RNA-Seq_1.fastq.gz"
+# second five
+sample="SRR5998593_GSM2769688_Ctrl_4h_3_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998597_GSM2769692_Ctrl_24h_1_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998598_GSM2769693_Ctrl_24h_2_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998599_GSM2769694_Ctrl_24h_3_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998603_GSM2769698_Ctrl_72h_1_Homo_sapiens_RNA-Seq_1.fastq.gz"
+# third five
+#sample="SRR5998604_GSM2769699_Ctrl_72h_2_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998605_GSM2769700_Ctrl_72h_3_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998609_GSM2769704_Ctrl_144h_1_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998610_GSM2769705_Ctrl_144h_2_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998611_GSM2769706_Ctrl_144h_3_Homo_sapiens_RNA-Seq_1.fastq.gz"
 
 for r1 in $sample
 do

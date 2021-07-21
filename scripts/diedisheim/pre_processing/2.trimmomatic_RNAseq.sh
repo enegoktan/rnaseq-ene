577aab7a66ef07027db380f911c2a2e0dad24737
enegoktan <goktan.ene@gmail.com>
2021-07-21 14:30:20 +0100

    add three sets of five instead of one big loop

--- scripts/diedisheim/pre_processing/2.trimmomatic_RNAseq.sh
+++ scripts/diedisheim/pre_processing/2.trimmomatic_RNAseq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes 1
 #SBATCH --ntasks 8                 # Number of nodes. Ensure that all cores are on one machine
-#SBATCH --time 2-00:0              # Runtime in D-HH:MM
+#SBATCH --time 0-04:0              # Runtime in D-HH:MM
 #SBATCH -o /rds/projects/t/thomaspz-fa-rna-seq/Ene/data/slurm_outputs/slurm_out/2.trimmomatic_RNAseq/trim_%j.out.txt      # File to which STDOUT will be written
 #SBATCH -e /rds/projects/t/thomaspz-fa-rna-seq/Ene/data/slurm_outputs/slurm_errors/2.trimmomatic_RNAseq/trim_%j.err.txt      # File to which STDERR will be written
 
@@ -16,7 +16,8 @@ module load MultiQC/1.9-foss-2019b-Python-3.7.4
 cd /rds/projects/t/thomaspz-fa-rna-seq/Ene/data/diedisheim/dataset/test_trim
 
 # Clip adapters
-sample=*RNA-Seq_1.fastq.gz
+#sample=*RNA-Seq_1.fastq.gz
+sample="SRR5998588_GSM2769683_Ctrl_0h_1_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998589_GSM2769684_Ctrl_0h_2_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998590_GSM2769685_Ctrl_0h_3_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998591_GSM2769686_Ctrl_4h_1_Homo_sapiens_RNA-Seq_1.fastq.gz SRR5998592_GSM2769687_Ctrl_4h_2_Homo_sapiens_RNA-Seq_1.fastq.gz"
 
 for r1 in $sample
 do
@@ -26,7 +27,8 @@ do
 done
 
 # Clip polyA tails
-sample=*RNA-Seq_TEMP_1P*
+#sample=*RNA-Seq_TEMP_1P*
+sample="SRR5998588_GSM2769683_Ctrl_0h_1_Homo_sapiens_RNA-Seq_TEMP_1P.fastq.gz SRR5998589_GSM2769684_Ctrl_0h_2_Homo_sapiens_RNA-Seq_TEMP_1P.fastq.gz SRR5998590_GSM2769685_Ctrl_0h_3_Homo_sapiens_RNA-Seq_TEMP_1P.fastq.gz SRR5998591_GSM2769686_Ctrl_4h_1_Homo_sapiens_RNA-Seq_TEMP_1P.fastq.gz SRR5998592_GSM2769687_Ctrl_4h_2_Homo_sapiens_RNA-Seq_TEMP_1P.fastq.gz"
 
 for r1 in $sample
 do
@@ -39,11 +41,11 @@ done
 #java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar PE -trimlog Triming_LOG_Trimmomatic2.fastq  temp_1P.fastq temp_2P.fastq -baseout trimTC${file} ILLUMINACLIP:/rds/projects/t/thomaspz-fa-rna-seq/Ene/scripts_and_manuals/scripts/Additional_files/polyA.fa:3:10:8:6 SLIDINGWINDOW:4:15 MINLEN:30
 
 #then test the trimmed and clipped file
-gunzip *RNA-Seq_trimmed*
+#gunzip *RNA-Seq_trimmed*
 
-for file in *RNA-Seq_trimmed*
-do
-  fastqc ${file}  -o /rds/projects/t/thomaspz-fa-rna-seq/Ene/data/diedisheim/2.trimmed_fastQC
-done
+#for file in *RNA-Seq_trimmed*
+#do
+#  fastqc ${file}  -o /rds/projects/t/thomaspz-fa-rna-seq/Ene/data/diedisheim/2.trimmed_fastQC
+#done
 
-gzip *
+#gzip *
